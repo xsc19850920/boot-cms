663a987b0d9b3e2cff37d289914c0e7d85eeecb1
xsc19850920 <xsc19850920@163.com>
2018-03-22 08:26:03 +0800

    commit for branch

--- src/main/java/com/vigekoo/common/datasource/DataSourceNames.java
+++ src/main/java/com/vigekoo/common/datasource/DataSourceNames.java
@@ -1,12 +1,12 @@
-package com.vigekoo.common.datasource;
-
-/**
- * @author sxia
- * @Description: TODO(配置多数据源的名字)
- * @date 2017-9-26 16:58
- */
-public interface DataSourceNames {
-
-    String FIRST = "first";
-
-}
+//package com.vigekoo.common.datasource;
+//
+///**
+// * @author sxia
+// * @Description: TODO(配置多数据源的名字)
+// * @date 2017-9-26 16:58
+// */
+//public interface DataSourceNames {
+//
+//    String FIRST = "first";
+//
+//}
--- src/main/java/com/vigekoo/config/DynamicDataSourceConfig.java
+++ src/main/java/com/vigekoo/config/DynamicDataSourceConfig.java
@@ -1,8 +1,8 @@
 package com.vigekoo.config;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
-import com.vigekoo.common.datasource.DataSourceNames;
-import com.vigekoo.common.datasource.DynamicDataSource;
+//import com.vigekoo.common.datasource.DataSourceNames;
+//import com.vigekoo.common.datasource.DynamicDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,17 +21,18 @@ import java.util.Map;
 public class DynamicDataSourceConfig {
 
     @Bean
-    @ConfigurationProperties("spring.datasource.druid.first")
+//    @ConfigurationProperties("spring.datasource.druid.first")
+    @ConfigurationProperties("spring.datasource.druid")
     public DataSource firstDataSource(){
         return DruidDataSourceBuilder.create().build();
     }
 
-    @Bean
-    @Primary
-    public DynamicDataSource dataSource(DataSource firstDataSource) {
-        Map<String, DataSource> targetDataSources = new HashMap<>();
-        targetDataSources.put(DataSourceNames.FIRST, firstDataSource);
-        return new DynamicDataSource(firstDataSource, targetDataSources);
-    }
+//    @Bean
+//    @Primary
+//    public DynamicDataSource dataSource(DataSource firstDataSource) {
+//        Map<String, DataSource> targetDataSources = new HashMap<>();
+//        targetDataSources.put(DataSourceNames.FIRST, firstDataSource);
+//        return new DynamicDataSource(firstDataSource, targetDataSources);
+//    }
 
 }
